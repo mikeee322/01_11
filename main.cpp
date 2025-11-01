e7611a593d2c2fc91658f9122e86946c4fa67987
l.misha.war2000@mail.ru <mikeee322>
2025-11-01 13:07:49 +0300

    make commit

--- main.cpp
+++ main.cpp
@@ -1,14 +1,5 @@
 #include <iostream>
 
-int ** make (int rows, int cols);
-
-void output (const int * const * mtx);
-void rm (int ** mtx, int rows, int cols){
-    for (size_t i = 0; i < rows; ++i){
-        delete [] mtx [i];
-    }
-    delete [] mtx;
-}
 
 int main(){
     int rows = 0;
@@ -17,9 +8,36 @@ int main(){
     if (std::cin.fail()){
         return 1;
     }
-
     int ** mtx = nullptr;
+    try{
+    
     mtx = make (rows, cols);
+    } catch (const std::bad_alloc&){
+        rm(mtx, rows);
+        return 2;
+    }
     output(mtx);
     rm (mtx);
+}
+
+int ** make (int rows, int cols){
+    int ** mtx = new int *[rows];
+    for (size_t i = 0; i < rows; ++i){
+        try{
+            mtx[i] = new int [cols];
+        }
+        catch(...){
+            rm(mtx, i);
+            throw;
+        }
+    }
+    return mtx;
+}
+
+void output (const int * const * mtx);
+void rm (int ** mtx, int rows, int cols){
+    for (size_t i = 0; i < rows; ++i){
+        delete [] mtx [i];
+    }
+    delete [] mtx;
 }
