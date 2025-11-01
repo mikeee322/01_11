d99c77b152c4cbb8866cecabd4ade37d11361388
l.misha.war2000@mail.ru <mikeee322>
2025-11-01 12:38:28 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int ** make (int rows, int cols);
+
+void output (const int * const * mtx);
+void rm (int ** mtx, int rows, int cols);
+
+int main(){
+    int rows = 0;
+    int cols = 0;
+    std::cin >> rows >> cols;
+    if (std::cin.fail()){
+        return 1;
+    }
+
+    int ** mtx = nullptr;
+    mtx = make (rows, cols);
+    output(mtx);
+    rm (mtx);
+}
